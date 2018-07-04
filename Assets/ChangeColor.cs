eab61e06d7a8b75a4528c2c9fcfd54743eeff102
lcd11001 <lcd11001@gmail.com>
2018-07-04 12:25:01 +0700

    + Picker plane for camera

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+	public void Change_Color(string colorRGB)
+	{
+		Material mat = this.gameObject.GetComponent<MeshRenderer>().material;
+		int colorHexaRGB = System.Int32.Parse(colorRGB.ToLower().Replace("0x", ""), System.Globalization.NumberStyles.HexNumber);
+		int r = (colorHexaRGB >> 16) & 0xFF;
+		int g = (colorHexaRGB >> 8) & 0xFF;
+		int b = (colorHexaRGB & 0xFF);
+		mat.color = new Color(r, g, b);
+	}
+}
---
+++ Assets/WebCamDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	private WebCamTexture wct;
+
+	void Start () 
+	{
+		wct = new WebCamTexture();
+		this.GetComponent<MeshRenderer>().material.mainTexture = wct;
+	}
+
+	public void Play()
+	{
+		wct.Play();
+	}
+
+	public void Stop()
+	{
+		wct.Stop();
+	}
+}
