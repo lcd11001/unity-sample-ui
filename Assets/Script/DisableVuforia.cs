041f589054fc636f0150c6717ebc1c97c68f1090
Luong Cong Dan <lcd11001@gmail.com>
2018-07-17 15:03:46 +0700

    + DisableVuforia

---
+++ Assets/Script/DisableVuforia.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.XR;
+
+public class DisableVuforia : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		Setup(false);
+	}
+
+	public void Setup(bool enable)
+	{
+		XRSettings.enabled = enable;
+		if (VuforiaBehaviour.Instance)
+		{
+			VuforiaBehaviour.Instance.enabled = enable;
+		}
+	}
+}
---
+++ Assets/Script/ScaleObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleObject : MonoBehaviour {
+
+	// Use this for initialization
+	public void Scale(float ratio)
+	{
+		transform.localScale = Vector3.one * ratio;
+	}
+}
