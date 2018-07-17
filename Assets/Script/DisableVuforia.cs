d427ebeb3eb52009d4a0c4147f4fec150c93f340
Luong Cong Dan <lcd11001@gmail.com>
2018-07-17 15:26:50 +0700

    + DisableVuforia

--- Assets/Script/DisableVuforia.cs
+++ Assets/Script/DisableVuforia.cs
@@ -7,16 +7,32 @@ using UnityEngine.XR;
 public class DisableVuforia : MonoBehaviour {
 
 	// Use this for initialization
-	void Awake () {
+	public Camera AR_Camera;
+	void Start () {
 		Setup(false);
 	}
 
 	public void Setup(bool enable)
 	{
+		StartCoroutine(_Setup(enable));
+	}
+
+	private IEnumerator _Setup(bool enable)
+	{
+		Debug.Log ("DelayFunction");
+		yield return new WaitForSeconds(3f); // waits 3 seconds
+
+		Debug.Log ("DelayFunction after 3 seconds");
+
 		XRSettings.enabled = enable;
 		if (VuforiaBehaviour.Instance)
 		{
 			VuforiaBehaviour.Instance.enabled = enable;
+		}
+		if (AR_Camera)
+		{
+			AR_Camera.gameObject.SetActive(enable);
+			AR_Camera.enabled = enable;
 		}
 	}
 }
