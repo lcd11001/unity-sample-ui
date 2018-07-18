75b5e8903737fb7fe8067393431e87d16271ce29
Luong Cong Dan <>
2018-07-18 14:48:59 +0700

    * camera mirror Y

--- Assets/Script/WebCamDisplay.cs
+++ Assets/Script/WebCamDisplay.cs
@@ -5,104 +5,123 @@ using UnityEngine.UI;
 
 // https://docs.unity3d.com/ScriptReference/WebCamTexture-videoRotationAngle.html
 
-public class WebCamDisplay : MonoBehaviour {
+public class WebCamDisplay : MonoBehaviour
+{
 
-	// Use this for initialization
-	public GameObject picker;
-	private Quaternion baseRotation;
-	private WebCamTexture wct;
-	private bool isPlaying;
-	private int centerX, centerY;
-	private int camW, camH;
-	private bool camEnable;
-
-#if UNITY_EDITOR
-	private Color camFakeColor;
-	private float camTimer;
-#endif
-
-	void Awake () 
-	{
-		camEnable = WebCamTexture.devices.Length > 0;
-		if (camEnable)
-		{
-			camW = Screen.width;
-			camH = Screen.height;
-
-			wct = new WebCamTexture(camW, camH);
-			centerX = camW / 2;
-			centerY = camH / 2;
-
-			this.GetComponent<MeshRenderer>().material.mainTexture = wct;
-		}
-		
-		isPlaying = false;
+    // Use this for initialization
+    public GameObject picker;
+    private Quaternion baseRotation;
+    private WebCamTexture wct;
+    private bool isPlaying;
+    private int centerX, centerY;
+    private int camW, camH;
+    private bool camEnable;
+    private bool hasMirrorY;
 
 #if UNITY_EDITOR
-		camFakeColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    private Color camFakeColor;
+    private float camTimer;
 #endif
-	}
 
-	void Start() 
-	{
-		baseRotation = transform.rotation;
-		// Debug.Log("baseRotation " + baseRotation);
-	}
+    void Awake()
+    {
+        camEnable = WebCamTexture.devices.Length > 0;
+        if (camEnable)
+        {
+            camW = Screen.width;
+            camH = Screen.height;
 
-	public void Play()
-	{
-		if (camEnable)
-		{
-			wct.Play();
-			isPlaying = true;
-		}
+            wct = new WebCamTexture(camW, camH);
+            centerX = camW / 2;
+            centerY = camH / 2;
+
+            this.GetComponent<MeshRenderer>().material.mainTexture = wct;
+        }
+
+        isPlaying = false;
 
 #if UNITY_EDITOR
-		camTimer = 0;
+        camFakeColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
 #endif
-	}
+    }
 
-	public void Stop()
-	{
-		if (camEnable)
-		{
-			isPlaying = false;
-			wct.Stop();
-		}
-		
-	}
+    void Start()
+    {
+        baseRotation = transform.rotation;
+        // Debug.Log("baseRotation " + baseRotation);
+    }
 
-	private void Update() 
-	{
+    public void Play()
+    {
+        if (camEnable)
+        {
+            wct.Play();
+            isPlaying = true;
+        }
+
+        hasMirrorY = false;
+
 #if UNITY_EDITOR
-		if (!camEnable)
-		{
-			camTimer += Time.deltaTime;
-			if (camTimer > 3)
-			{
-				camFakeColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-				ApplyColor(camFakeColor);
-
-				camTimer = 0;
-			}
-
-			return;
-		}
+        camTimer = 0;
 #endif
-		if (isPlaying)
-		{
-			ApplyColor( wct.GetPixel(centerX, centerY) );
+    }
 
-			transform.rotation = baseRotation * Quaternion.AngleAxis(wct.videoRotationAngle, Vector3.up);
-		}
-	}
+    public void Stop()
+    {
+        if (camEnable)
+        {
+            isPlaying = false;
+            wct.Stop();
+        }
 
-	private void ApplyColor(Color c)
-	{
-		if (picker)
-		{
-			Image img = picker.GetComponent<Image>();
-			img.color = c;
-		}
-	}
+        if (hasMirrorY)
+        {
+            Vector3 mirrorY = transform.localScale;
+            mirrorY.z *= -1;
+            transform.localScale = mirrorY;
+            hasMirrorY = false;
+        }
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (!camEnable)
+        {
+            camTimer += Time.deltaTime;
+            if (camTimer > 3)
+            {
+                camFakeColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+                ApplyColor(camFakeColor);
+
+                camTimer = 0;
+            }
+
+            return;
+        }
+#endif
+        if (isPlaying)
+        {
+            ApplyColor(wct.GetPixel(centerX, centerY));
+
+            transform.rotation = baseRotation * Quaternion.AngleAxis(wct.videoRotationAngle, Vector3.up);
+
+            if (wct.videoVerticallyMirrored && !hasMirrorY)
+            {
+                Vector3 mirrorY = transform.localScale;
+                mirrorY.z *= -1;
+                transform.localScale = mirrorY;
+                hasMirrorY = true;
+            }
+        }
+    }
+
+    private void ApplyColor(Color c)
+    {
+        if (picker)
+        {
+            Image img = picker.GetComponent<Image>();
+            img.color = c;
+        }
+    }
 }
